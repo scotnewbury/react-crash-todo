775a81976ec446d3e6c206abffdcfd078cfd5690
scotnewbury <scot@scotnewbury.com>
2019-05-19 16:52:35 -0400

    Added basic header to list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import Todos from './components/Todos'
+import Header from './components/layout/Header';
+import Todos from './components/Todos';
 import './App.css';
 
 class App extends Component {
@@ -39,6 +40,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <Todos todos={this.state.todos} toggleComplete={this.toggleComplete}
           delTodo={this.delTodo}
         />
